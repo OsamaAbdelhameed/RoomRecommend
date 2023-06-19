2860c9153eeda18089c2e1cdae602125cfe866da
OsamaAbdelhameed <osamaabdelhameed41@gmail.com>
2023-06-19 12:32:38 +0800

    update data

--- RoomRecommend.py
+++ RoomRecommend.py
@@ -25,7 +25,7 @@ y = y[X.index]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Create the KNN classifier
-knn = KNeighborsClassifier(n_neighbors=2)
+knn = KNeighborsClassifier(n_neighbors=2) 
 
 # Train the model
 knn.fit(X_train, y_train)
